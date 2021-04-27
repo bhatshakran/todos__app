e1aeb702f23014f24a4afed1727b2726f57f6bcf
bhat shakran <bhatshakran@gmail.com>
2021-04-27 22:11:22 +0530

    added aws authentication

--- src/App.js
+++ src/App.js
@@ -1,12 +1,13 @@
-import logo from './logo.svg';
 import './App.css';
+import { withAuthenticator, AmplifySignOut } from '@aws-amplify/ui-react';
 
 function App() {
 	return (
 		<div className='App'>
 			<header className='App-header'>Hello from aws!</header>
+			<AmplifySignOut />
 		</div>
 	);
 }
 
-export default App;
+export default withAuthenticator(App);
